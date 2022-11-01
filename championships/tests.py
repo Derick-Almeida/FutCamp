759bd8a3e16fbbd0bc137007048d9aa2dab260c2
Gabriel Pereira <ggabriel.p2003@gmail.com>
2022-11-01 14:39:39 -0300

    feat/adicionando rota championship

--- championships/tests.py
+++ championships/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from users.models import User
+from .models import Championship
 
-# Create your tests here.
+
+class Test_Championship(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.admin_obj = {}
+        cls.admin_data = User.objects.create_superuser(**cls.admin_obj)
+
+    def test_create_championship_and_possibles_errors(self):
+
+        championships_obj = {}
+        championships_data = Championship.objects.create(**championships_obj)
+
+        championships_obj_error = {}
+        championships_obj_error_data = Championship.objects.create(
+            **championships_obj_error
+        )
+
+        result_status_error = championships_obj_error_data.status_code
+        expected_status_error = 400
+
+        result_status = championships_data.status_code
+        expected_status = 201
+
+        self.assertEqual(result_status_error, expected_status_error)
+        self.assertEqual(result_status, expected_status)
--- championships/views.py
+++ championships/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAdminUser
+from .models import Championship
+from championships.serializer import ChampionshipSerializer
 
-# Create your views here.
+
+class ChampionshipView(generics.ListCreateAPIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
+
+    serializer_class = ChampionshipSerializer
+    queryset = Championship
+
+
+class ChampionshipDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
+
+    serializer_class = ChampionshipSerializer
+    queryset = Championship
+
+    lookup_url_kwarg = "championships_id"
